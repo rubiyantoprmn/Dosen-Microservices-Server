e29374ed1ed7c4d1cf0689b63e572018e03570a5
Muhammad Rubiyanto Permana <rubiyanto.permana@gmail.com>
2017-05-31 09:18:53 +0700

    Update file application.yml dan PegawaiController.java
    application.yml mengubah username dan password database
    PegawaiController.java mengubah request mapping

--- src/main/java/com/doj/dosen/DosenMicroservicesServer/PegawaiController.java
+++ src/main/java/com/doj/dosen/DosenMicroservicesServer/PegawaiController.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,21 +19,21 @@ protected Logger logger = Logger.getLogger(PegawaiController.class.getName());
 	@Autowired
 	PegawaiRepository repository;
 	
-	@RequestMapping("/pegawai")
-	public Pegawai[] All()
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Pegawai> findAll()
 	{
-		logger.info("pegawai-microservice all() invoked");
+		//logger.info("mahasiswa-microservice all() invoked");
 		List<Pegawai> pegawai = PegawaiRepository.getAllPegawai();
-		logger.info("pegawai-microservice all() found: " + pegawai.size());
-		return pegawai.toArray(new Pegawai[pegawai.size()]);
+		//logger.info("mahasiswa-microservice all() found: " + mahasiswa.size());
+		return pegawai;
 	}
 	
-	@RequestMapping("/pegawai/{id}")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Pegawai ById(@PathVariable("nip") String id)
 	{
-		logger.info("pegawai-microservice byId() invoked: " + id);
+		//logger.info("mahasiswa-microservice byId() invoked: " + id);
 		Pegawai pegawai = PegawaiRepository.getPegawai(id);
-		logger.info("pegawai-microservice byId() found: " + pegawai);
+		//logger.info("mahasiswa-microservice byId() found: " + mahasiswa);
 		return pegawai;
 	}
 
