3abc2c3ceab764d59350256cf47f98d304fec49f
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-13 09:03:42 +0700

    Remove Eureka import

--- src/main/java/com/doj/dosen/DosenMicroservicesServer/DosenMicroservicesServerApplication.java
+++ src/main/java/com/doj/dosen/DosenMicroservicesServer/DosenMicroservicesServerApplication.java
@@ -2,7 +2,6 @@ package com.doj.dosen.DosenMicroservicesServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class DosenMicroservicesServerApplication {
